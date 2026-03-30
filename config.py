9ca1bd6b8f226122517be977d70e1002d79a2f08
Honore <harerimana456@gmail.com>
2026-03-30 08:27:25 +0200

    added actual url

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
 
     # Database
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL_TESTING')
+        'DATABASE_URL')
     
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get(
         'SQLALCHEMY_TRACK_MODIFICATIONS', 'False'
